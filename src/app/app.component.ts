b4af2705f034d0a9dc0b06ee87328a6f1e17b305
long <nguyenthanhlong@morenergy.net>
2024-06-12 12:51:17 +0200

    feat: scroll chatbot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatInputModule} from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
@@ -17,7 +17,41 @@ import {MatDividerModule} from '@angular/material/divider';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewChecked {
+  @ViewChild('messagesContainer') private messagesContainer: ElementRef;
   title = 'ollama-chatbot';
   items = Array.from({ length: 50 }, (_, i) => i + 1); // Create an array of numbers from 1 to 50
+  yourName = 'You'
+  botName = 'Ollama'
+  userInput: string = '';
+
+  sentMessages: string[] = [];
+  constructor() {}
+
+  ngAfterViewChecked() {
+    this.scrollToBottom();
+  }
+
+  onSubmit() {
+    console.log('User input:', this.userInput)
+    /** Send POST to API here. */
+    this.sentMessages.push(this.userInput);
+    
+
+    this.userInput = ''; // Clear the user input after processing
+  }
+
+  scrollToBottom(): void {
+    try {
+      this.messagesContainer.nativeElement.scrollTop = this.messagesContainer.nativeElement.scrollHeight;
+    } catch(err) {
+      console.error('Scroll error:', err);
+    }
+  }
+
+  onNewChat() {
+    this.sentMessages = [];
+    this.userInput = '';
+  }
+
 }
